35e959039e2c095c320758341ca892c4a88c3da8
David Diaz <david.diaz@goto.com>
2023-01-06 12:42:15 -0600

    Template has been updated

--- js/create.js
+++ js/create.js
@@ -31,7 +31,6 @@ function createTemplate(){
 
     //alert("Creating templates for: " + selected);
 
-
     //FYI: IMPACT == ISSUE!
 
     // This will be our first template - External Status Page Template:
@@ -39,17 +38,25 @@ function createTemplate(){
 
     var tab = window.open('about:blank', '_blank');
 
-    html = '<title>Results - Incidents Templates</title><br><strong> External Status Page Template</strong><br><br>' + selected + ' - ' + cTittle
+    html = '<title>Results - Incidents Templates</title><br><strong> External Status Page Template</strong><br><br>' + 
+    selected + ' - ' + cTittle
     + '<br><br>We are actively investigating reports that some ' + selected + ' customers may be '
     + cImpact + '. <br><br>Our engineers are working to identify the issue and will provide another update shortly.' +  
+    
+    '<br><br><strong>Internal Status Page Template:</strong><br><br>' + 
+    finalTitle + 
+    '<br><br>Our engineers are investigating a ' + cIncidentType + ' incident for ' + selected +
+    '<br><br><b>Customer Impact - </b>' + cTittle +
+    '<br><b>Customer Reports - </b>' + cImpact +
+    '<br><b>Slack Channel - </b>' + cSlackChannel;
 
-    '<br><br><strong>Internal Status Page Template:</strong><br><br>' + finalTitle + '<br><br></bnr>Product Impact - ' + selected +
-    '<br>Customer Reports - ' + cReports + '<br>Customer Impact - Customers may be ' + cImpact + '<br>Slack Channel - ' + cSlackChannel + '<br>Conf. Bridge - ' + 
-    confBridge + ' ' + '<a href="https://www.uberconference.com/cloudsupport">https://www.uberconference.com/cloudsupport</a>' +
-    '<br><br><strong>Comms Response Template:</strong><br><br>We are currently having a ' + cIncidentType + ' incident for ' + selected + ' where users are ' + cImpact + '.' +
-    '<br>Status pages have been sent. Please thread the questions in this message.<br>Slack incident channel ' + cSlackChannel + 
-    '<br><br>Your friends:<br><img src="images/lminoclogo.png" alt="NOC_Logo" width="124" height="150">' +
-    '<br><br><strong>DONT FORGET TO SEND THE SLACK UPDATES</strong>'
+
+    // '<br><br>Customer Reports - ' + cReports + '<br>Customer Impact - Customers may be ' + cImpact + '<br>Slack Channel - ' + cSlackChannel + '<br>Conf. Bridge - ' + 
+    // confBridge + ' ' + '<a href="https://www.uberconference.com/cloudsupport">https://www.uberconference.com/cloudsupport</a>' +
+    // '<br><br><strong>Comms Response Template:</strong><br><br>We are currently having a ' + cIncidentType + ' incident for ' + selected + ' where users are ' + cImpact + '.' +
+    // '<br>Status pages have been sent. Please thread the questions in this message.<br>Slack incident channel ' + cSlackChannel + 
+    // '<br><br>Your friends:<br><img src="images/lminoclogo.png" alt="NOC_Logo" width="124" height="150">' +
+    // '<br><br><strong>DONT FORGET TO SEND THE SLACK UPDATES</strong>'
 
     tab.document.write(html);
     tab.document.close(); 
@@ -71,27 +78,27 @@ function createTemplateComms(){
 
     // Lets get our Start and End time
 
-    var getStartTime = document.getElementById("startDateTime").value;
+    //var getStartTime = document.getElementById("startDateTime").value;
 
     //console.log("My getStartTime BEFORE: " + getStartTime);
 
-    var newStartTime = fixDate(getStartTime);
+    //var newStartTime = fixDate(getStartTime);
 
     //console.log('New start time: ' +  newStartTime);
 
     // Now let's get our End time
     
-    var getEndTime = document.getElementById("endDateTime").value;
+    //var getEndTime = document.getElementById("endDateTime").value;
 
     //console.log("End Time BEFORE: " + getEndTime);
 
-    var newEndTime = fixDate(getEndTime);
+    //var newEndTime = fixDate(getEndTime);
 
     //console.log('New end time: ' +  newEndTime);
 
     // For example: 10/25/2022 9:00 AM UTC - 10/26/2022 04:39 PM UTC
 
-    var finalTimeline = newStartTime + " UTC - " + newEndTime + " UTC";
+    //var finalTimeline = newStartTime + " UTC - " + newEndTime + " UTC";
 
     // End of fixing date format
 
@@ -115,29 +122,28 @@ function createTemplateComms(){
 
     }
 
+    // This will create the CLOSING statements
 
     var tab = window.open('about:blank', '_blank');
 
     html = '<title>Results - Internal Status Page & Comms Response</title>'
     + '<h1><b>Comms Response - Closing Statement</b></h1>'
     + '<b>' + isProductPlural + ' Impact: </b>' + selected
-    + '<br><b>Timeline: </b>' + finalTimeline
+    //+ '<br><b>Timeline: </b>' + finalTimeline
     + '<br><b>Customer Impact: </b>' + cImpact
     + '<br><b>Customer Reports: </b>' + cReports
     + '<br><b>Root Cause: </b>' + getRootCause
     + '<br><b>Incident Status Update </b> - Resolved'
-    + '<br><b>What was done to resolve the issue? </b>\n' + getFix
+    + '<br><b>What was done to resolve the issue? </b>' + getFix
     + '<br><b>RFO</b> - Upon request, RFO for external customer communication will be published to the RFO distribution list within 48-72 hours.'
     + '<br><b>PostMortem/RCA</b> - A detailed technical doc will be shared in the incident Slack Channel ' + cSlackChannel
     
     + '<h1><b>Internal Status Page - Closing Statement</b></h1>'
-    + 'On ' + finalTimeline + ',' + selected 
-    + ' customers may have been ' + cImpact 
-    + '. This was identified to have been caused due to ' + getRootCause + '.'
-    + ' To mitigate the issue, ' + getFix
-    + '<br><br><b>RFO</b> - Upon request, RFO for external customer communication will be published to the RFO distribution list within 48-72 hours.'
-    + '<br><br><b>PostMortem/RCA</b> - A detailed technical doc will be shared in the incident Slack Channel ' + cSlackChannel;
-
+    + 'We have confirmed that the issue has been resolved and that all systems are 100% operational.'
+    + '<br><br><b>Final Customer Reports - </b>' + cReports
+    + '<br><b>Root Cause - </b>' + getRootCause
+    + '<br><b>Resolution - </b>' + getFix
+    + '<br><b>PostMortem - </b>A detailed technical document will be shared in the incident Slack Channel ' + cSlackChannel;
 
     tab.document.write(html);
     tab.document.close();
@@ -169,6 +175,8 @@ function createDraft(){
     tab.document.close(); 
 
 }
+
+// This function could be used later on, will leave it here in case we need it afterwards.
 
 function fixDate(getDate){
 
